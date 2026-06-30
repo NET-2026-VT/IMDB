f01eb26f7012d8cc75af85a496287ff1f96f0612
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2026-06-30 10:44:01 +0200

    Annotations

---
+++ IMDB/Data/Migrations/20260630083600_Annotations.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IMDB.Migrations
+{
+    /// <inheritdoc />
+    public partial class Annotations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Movie",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Movie",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20);
+        }
+    }
+}
--- IMDB/Models/Entities/Movie.cs
+++ IMDB/Models/Entities/Movie.cs
@@ -1,10 +1,19 @@
-﻿namespace IMDB.Models.Entities;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IMDB.Models.Entities;
 
 public class Movie
 {
     public int Id { get; set; }
-    public required string Title { get; set; } 
+
+    [StringLength(20)]
+    public required string Title { get; set; }
+
+    [DataType(DataType.Date)]
+    [Display(Name = "Release Date")]
     public required DateTime ReleaseDate { get; set; }
     public Genre Genre { get; set; }
+
+    [Range(1, 10)]
     public int Rating { get; set; }
 }
