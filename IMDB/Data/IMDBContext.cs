36849bca33ffe4def3fe79e231784945b6d811c9
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2026-06-30 11:03:22 +0200

    Seed

--- IMDB/Data/IMDBContext.cs
+++ IMDB/Data/IMDBContext.cs
@@ -8,4 +8,61 @@ public class IMDBContext : DbContext
     public IMDBContext(DbContextOptions<IMDBContext> options) : base(options)
     {
     }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Movie>().HasData
+            (
+                 new Movie
+                 {
+                     Id = 1,
+                     Title = "Titanic",
+                     Genre = Genre.Drama,
+                     ReleaseDate = new DateTime(1997, 1, 16),
+                     Rating = 7.9F
+                 },
+            new Movie
+            {
+                Id = 2,
+                Title = "Scream",
+                Genre = Genre.Horror,
+                ReleaseDate = new DateTime(1997, 8, 1),
+                Rating = 7.4F
+            },
+            new Movie
+            {
+                Id = 3,
+                Title = "The Shining",
+                Genre = Genre.Horror,
+                ReleaseDate = new DateTime(1980, 9, 26),
+                Rating = 8.4F
+            },
+            new Movie
+            {
+                Id = 4,
+                Title = "300",
+                Genre = Genre.Action,
+                ReleaseDate = new DateTime(2007, 4, 4),
+                Rating = 7.6F
+            },
+            new Movie
+            {
+                Id = 5,
+                Title = "Interstellar",
+                Genre = Genre.Drama,
+                ReleaseDate = new DateTime(2014, 11, 7),
+                Rating = 8.7F
+            },
+            new Movie
+            {
+                Id = 6,
+                Title = "The Dark Knight",
+                Genre = Genre.Action,
+                ReleaseDate = new DateTime(2008, 7, 25),
+                Rating = 9.1F
+            }
+            );
+    }
 }
---
+++ IMDB/Data/Migrations/20260630085911_Seed.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace IMDB.Migrations
+{
+    /// <inheritdoc />
+    public partial class Seed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Movie",
+                columns: new[] { "Id", "Genre", "Rating", "ReleaseDate", "Title" },
+                values: new object[,]
+                {
+                    { 1, 3, 7.9f, new DateTime(1997, 1, 16, 0, 0, 0, 0, DateTimeKind.Unspecified), "Titanic" },
+                    { 2, 0, 7.4f, new DateTime(1997, 8, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Scream" },
+                    { 3, 0, 8.4f, new DateTime(1980, 9, 26, 0, 0, 0, 0, DateTimeKind.Unspecified), "The Shining" },
+                    { 4, 4, 7.6f, new DateTime(2007, 4, 4, 0, 0, 0, 0, DateTimeKind.Unspecified), "300" },
+                    { 5, 3, 8.7f, new DateTime(2014, 11, 7, 0, 0, 0, 0, DateTimeKind.Unspecified), "Interstellar" },
+                    { 6, 4, 9.1f, new DateTime(2008, 7, 25, 0, 0, 0, 0, DateTimeKind.Unspecified), "The Dark Knight" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Movie",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Movie",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Movie",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Movie",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Movie",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Movie",
+                keyColumn: "Id",
+                keyValue: 6);
+        }
+    }
+}
