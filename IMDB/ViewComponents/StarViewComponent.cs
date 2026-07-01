8baa7bbe2523932cd05e9c2028616adf22d7caad
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2026-07-01 11:59:34 +0200

    ViewComponent

---
+++ IMDB/ViewComponents/StarViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IMDB.ViewComponents;
+
+//[ViewComponent]
+public class StarViewComponent : ViewComponent
+{
+    public StarViewComponent()
+    {
+        //Add services as usual if needed
+    }
+
+    public IViewComponentResult Invoke(float rating)
+    {
+        var roundedRating = (int)Math.Floor(rating);
+
+        var model = new StarViewModel
+        {
+            Stars = roundedRating,
+            IsHalfStar = rating % 1 >= 0.5f
+        };
+
+        return View(model);
+    }
+}
---
+++ IMDB/ViewComponents/StarViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace IMDB.ViewComponents;
+
+internal class StarViewModel
+{
+    public string FullStarUrl =>
+      "https://upload.wikimedia.org/wikipedia/commons/e/e5/Full_Star_Yellow.svg";
+
+    public string HalfStarUrl =>
+        "https://upload.wikimedia.org/wikipedia/commons/d/d6/Half_Star_Yellow.svg";
+
+    public int Stars { get; set; }
+    public bool IsHalfStar { get; set; }
+}
